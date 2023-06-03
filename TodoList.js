d9c70c445752a144ad50c2f476ddb7b2b6d95131
jianhan1212 <zx156234820@gmail.com>
2023-06-03 22:39:42 +0800

    sidebar

--- TodoList.js
+++ TodoList.js
@@ -1,6 +1,8 @@
 const addBtn = document.getElementById('add-task');
 const input = document.getElementById('input');
 const taskContainer = document.getElementById('task-container');
+const toggle = document.querySelector('.bx-chevrons-right');
+const sidebar = document.querySelector('nav');
 
 addBtn.addEventListener('click', () => {
     let task = document.createElement('div')
@@ -9,14 +11,14 @@ addBtn.addEventListener('click', () => {
     let li = document.createElement('li')
     li.innerText = `${input.value}`
     task.appendChild(li)
-    
-    if (input.value === ""){
+
+    if (input.value === "") {
         alert("請輸入內容!!!")
     } else {
         taskContainer.appendChild(task)
     }
     input.value = ""//輸入完重置
-    
+
     let checkBtn = document.createElement('button')
     checkBtn.classList.add("checkBtn")
     checkBtn.innerHTML = `<i class="fa-regular fa-square"></i>`
@@ -32,8 +34,14 @@ addBtn.addEventListener('click', () => {
     deleteBtn.innerText = `del`
     task.appendChild(deleteBtn)
 
+    let fixBtn = document.createElement('button')
+    fixBtn.classList.add("fixBtn")
+    fixBtn.innerText = `fix`
+    task.appendChild(fixBtn)
+    fixBtn.hidden = true
+
     checkBtn.addEventListener('click', () => {
-        if( checkBtn.innerHTML === `<i class="fa-regular fa-square"></i>`) {
+        if (checkBtn.innerHTML === `<i class="fa-regular fa-square"></i>`) {
             checkBtn.innerHTML = `<i class="fa-regular fa-square-check"></i>`
             checkBtn.parentElement.style.backgroundColor = "darkgrey"
             checkBtn.style.backgroundColor = "red"
@@ -49,17 +57,34 @@ addBtn.addEventListener('click', () => {
     });
 
     editBtn.addEventListener('click', () => {
-        // let target = li.innerText
-
-        // console.log(target)
         let newinput = document.createElement('textarea')
+        newinput.classList.add("textarea")
+        let oldinput = li.innerText
         li.remove()
-        // li.innerHTML = `${newinput.value}`
+        newinput.value += oldinput
+        deleteBtn.hidden = true
+        editBtn.hidden = true
+        fixBtn.hidden = false
         task.appendChild(newinput)
-        console.log(newinput.value)
+        
+        fixBtn.addEventListener('click', () => {
+            let currentinput = `${newinput.value}`
+            newinput.remove()
+            let newli = document.createElement('li')
+            newli.innerText = currentinput
+            task.appendChild(newli)
+            deleteBtn.hidden = false
+            editBtn.hidden = false
+            fixBtn.hidden = true
+        });
+        
     })
+
 
 
 });
 
 
+toggle.addEventListener('click', () => {
+    sidebar.classList.toggle("close");
+});
